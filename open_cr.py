8c94c16f684cbd988b6ae1e990a8ca45f2e5305b
Jules Dupont <julesdupont02@gmail.com>
2020-08-04 22:59:59 +0200

    Deleted open_cr.py file (to be re-added)

--- open_cr.py
+++
@@ -1,7 +0,0 @@
-import tarfile
-
-tar = tarfile.open("filename.tar.gz", "r:gz")
-for member in tar.getmembers():
-     f = tar.extractfile(member)
-     if f is not None:
-         content = f.read()
